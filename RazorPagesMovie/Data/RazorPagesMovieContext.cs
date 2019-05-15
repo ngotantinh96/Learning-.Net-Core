32329b8608257c140b7d325729834db6fef8704b
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-15 14:06:32 +0700

    upload file

--- RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie.Models
 {
     public class RazorPagesMovieContext : DbContext
     {
-        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
             : base(options)
         {
         }
 
         public DbSet<Movie> Movie { get; set; }
+        public DbSet<Schedule> Schedule { get; set; }
     }
 }
---
+++ RazorPagesMovie/Models/Schedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Schedule
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string PublicSchedule { get; set; }
+
+        [Display(Name = "Public Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PublicScheduleSize { get; set; }
+
+        public string PrivateSchedule { get; set; }
+
+        [Display(Name = "Private Schedule Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N1}")]
+        public long PrivateScheduleSize { get; set; }
+
+        [Display(Name = "Uploaded (UTC)")]
+        [DisplayFormat(DataFormatString = "{0:F}")]
+        public DateTime UploadDT { get; set; }
+    }
+}
