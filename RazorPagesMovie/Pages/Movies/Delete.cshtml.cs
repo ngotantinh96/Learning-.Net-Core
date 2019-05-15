eca4514c56ca8fc9de93d0c7af448c5ccf1ea194
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-15 11:32:45 +0700

    working with route constraint

--- RazorPagesMovie/Pages/Movies/Delete.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Delete.cshtml.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Pages.Movies
 {
     public class DeleteModel : PageModel
     {
-        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+        private readonly RazorPagesMovieContext _context;
 
-        public DeleteModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        public DeleteModel(RazorPagesMovieContext context)
         {
             _context = context;
         }
@@ -25,10 +22,12 @@ namespace RazorPagesMovie.Pages.Movies
         {
             if (id == null)
             {
-                return NotFound();
+                Movie = await _context.Movie.FirstOrDefaultAsync();
             }
-
-            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            else
+            {
+                Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            }
 
             if (Movie == null)
             {
