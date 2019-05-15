cedb2955de4cbf215a433a6aa4e8b861fa8221a6
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-15 13:33:04 +0700

    add validation

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -2,20 +2,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RazorPagesMovie.Models
+public class Movie
 {
-    public class Movie
-    {
-        public int ID { get; set; }
-        public string Title { get; set; }
+    public int ID { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; }
 
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
-        public string Rating { get; set; }
-    }
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
+    public string Genre { get; set; }
+
+    [Range(1, 100), DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+    public string Rating { get; set; }
 }
