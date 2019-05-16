389db0c427b8c5f87bf31a2e86341b7413e53850
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-16 10:06:09 +0700

    pass model to tag helper

--- AuthoringTagHelpers/Controllers/HomeController.cs
+++ AuthoringTagHelpers/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AuthoringTagHelpers.Models;
 using Microsoft.AspNetCore.Mvc;
-using AuthoringTagHelpers.Models;
+using System;
+using System.Diagnostics;
 
 namespace AuthoringTagHelpers.Controllers
 {
@@ -17,7 +14,15 @@ namespace AuthoringTagHelpers.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            WebsiteContext webContext = new WebsiteContext
+            {
+                Version = new Version(1, 3),
+                CopyrightYear = 1638,
+                Approved = true,
+                TagsToShow = 131
+            };
+
+            return View(webContext);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
