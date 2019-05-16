aca86ded16be3b0be3b6344ef3a498066b35050b
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-16 11:03:30 +0700

    specify view component name

--- StarterViewComp/ViewComponents/PriorityListViewComponent.cs
+++ StarterViewComp/ViewComponents/PriorityListViewComponent.cs
@@ -18,8 +18,14 @@ namespace ViewComponentSample.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int maxPriority, bool isDone)
         {
+            string MyView = "Default";
+            // If asking for all completed tasks, render with the "PVC" view.
+            if (maxPriority > 3 && isDone == true)
+            {
+                MyView = "PVC";
+            }
             var items = await GetItemsAsync(maxPriority, isDone);
-            return View(items);
+            return View(MyView, items);
         }
         private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
         {
