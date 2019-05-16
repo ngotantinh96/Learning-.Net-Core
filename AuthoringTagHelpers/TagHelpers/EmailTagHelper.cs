8e7e4d2ffc4d743f20bd9f7e6d6b9cc63e44fbc7
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-16 09:42:03 +0700

    init project and make tag helper for email

---
+++ AuthoringTagHelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "@contoso.com";
+
+        // Can be passed via <email mail-to="..." />. 
+        // PascalCase gets translated into kebab-case.
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <email> with <a> tag
+
+            var address = MailTo + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
