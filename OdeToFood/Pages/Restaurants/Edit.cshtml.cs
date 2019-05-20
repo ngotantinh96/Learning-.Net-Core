c03d3de74d4853b9e071c910b6ca7ed8502c568d
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-20 16:02:10 +0700

    add edit page , cuisine select list

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFoo.Data;
+using OdeToFood.Core;
+using System.Collections.Generic;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
