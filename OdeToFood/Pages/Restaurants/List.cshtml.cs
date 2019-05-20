159b3ad2f5c6983679386af7c296e6aafd2b907f
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-20 13:48:58 +0700

    display restaurants

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using OdeToFoo.Data;
+using OdeToFood.Core;
+using System.Collections.Generic;
 
 namespace OdeToFood.Pages.Restaurants
 {
@@ -10,6 +12,7 @@ namespace OdeToFood.Pages.Restaurants
         private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
         public ListModel(
             IConfiguration configuration,
@@ -22,6 +25,7 @@ namespace OdeToFood.Pages.Restaurants
         public void OnGet()
         {
             Message = configuration["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
