b44685cd55cd70f8d9686a9558099d71fbed8d21
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-20 17:33:21 +0700

    add a connection string

---
+++ OdeToFoo.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFoo.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
