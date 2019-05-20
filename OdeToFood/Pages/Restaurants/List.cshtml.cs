75464a77c775977a4713815986dd61ef077e111e
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-20 13:14:37 +0700

    Inject and using configration from appsetting.json

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration configuration;
+
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void OnGet()
         {
-
+            Message = configuration["Message"];
         }
     }
 }
