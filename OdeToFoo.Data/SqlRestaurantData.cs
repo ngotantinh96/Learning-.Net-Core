3dbb3db4bf6d750f405d7dad57eb59614628d728
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-21 09:28:11 +0700

    add new class to work with db using EF and LINQ

---
+++ OdeToFoo.Data/SqlRestaurantData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFoo.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            db.Add(restaurant);
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = GetById(id);
+            if (restaurant != null)
+            {
+                db.Restaurants.Remove(restaurant);
+            }
+
+            return restaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return db.Restaurants.Find(id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return db.Restaurants
+                .Where(x => x.Name.StartsWith(name) || string.IsNullOrEmpty(name))
+                .OrderBy(x => x.Name);
+        }
+
+        public Restaurant Update(Restaurant restaurant)
+        {
+            var entity = db.Restaurants.Attach(restaurant);
+            entity.State = EntityState.Modified;
+            return restaurant;
+        }
+    }
+}
