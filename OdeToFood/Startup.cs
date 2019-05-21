8c59435801968d0abc45f37be71c82a3dbc31a2c
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-21 15:16:47 +0700

    add simple middleware

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -52,13 +52,24 @@ namespace OdeToFood
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.Use(SayHelloMiddleWare);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseNodeModules(env);
             app.UseCookiePolicy();
 
             app.UseMvc();
+        }
+
+        private RequestDelegate SayHelloMiddleWare(RequestDelegate next)
+        {
+            return async ctx =>
+            {
+                if (ctx.Request.Path.StartsWithSegments("/hello"))
+                    await ctx.Response.WriteAsync("Hello, World!");
+                else
+                    await next(ctx);
+            };
         }
     }
 }
