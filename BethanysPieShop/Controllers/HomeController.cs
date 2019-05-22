f6f84196de704f467997629d683f93e1511a5170
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-22 13:46:25 +0700

    add & and work around with _Layou _ViewStart ViewModel

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie Overview";
+
+            var pies = pieRepository.GetAllPies().OrderBy(p => p.Name).ToList();
+
+            var homeViewModel = new HomeViewModel
+            {
+                Pies = pies,
+                Title = "Pie Overview"
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShop.Models;
+using System.Collections.Generic;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
