ccd8236e5792b7790f8b220cc83a88ebd88834c5
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-22 15:21:31 +0700

    add dbcontext and register in configure services

---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext db;
+
+        public PieRepository(AppDbContext db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return db.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return db.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,17 +1,30 @@
 ﻿using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BethanysPieShop
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddTransient<IPieRepository, PieRepository>();
 
             services.AddMvc();
         }
