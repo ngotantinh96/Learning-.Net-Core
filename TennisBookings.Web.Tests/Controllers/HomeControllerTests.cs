08a52d5a9bff07bc8127d4c10b10a1bbd614baef
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-24 15:36:27 +0700

    add begin source for dependency injection

---
+++ TennisBookings.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisBookings.Web.Controllers;
+using TennisBookings.Web.ViewModels;
+using Xunit;
+
+namespace TennisBookings.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ReturnsExpectedViewModel_WhenWeatherIsSun()
+        {
+            var sut = new HomeController();
+
+            var result = sut.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<HomeViewModel>(viewResult.ViewData.Model);
+            Assert.Contains("It's sunny right now.", model.WeatherDescription);
+        }
+
+        [Fact]
+        public void ReturnsExpectedViewModel_WhenWeatherIsRain()
+        {
+            var sut = new HomeController();
+
+            var result = sut.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<HomeViewModel>(viewResult.ViewData.Model);
+            Assert.Contains("We're sorry but it's raining here.", model.WeatherDescription);
+        }
+    }
+}
