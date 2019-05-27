26c27fac25a3dcefb9ecd71b0c3648a2c3c3db8a
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-27 15:24:15 +0700

    first view component

---
+++ Globomantics/Components/MortgageRatesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Globomantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Globomantics.Components
+{
+    public class MortgageRatesViewComponent : ViewComponent
+    {
+        private readonly IRateService rateService;
+
+        public MortgageRatesViewComponent(IRateService rateService)
+        {
+            this.rateService = rateService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var rates = rateService.GetMortgageRates();
+
+            return View(rates);
+        }
+    }
+}
