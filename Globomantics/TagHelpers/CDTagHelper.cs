0e726dd9c80f65a27106daef891e2c4c1e20ee7e
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-27 14:56:17 +0700

    cd tag helper with DI

---
+++ Globomantics/TagHelpers/CDTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Globomantics.Core.Models;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Globomantics
+{
+    [HtmlTargetElement("cdrate")]
+    public class CDRateTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+
+        public string MeterPercent { get; set; }
+
+        public CDTermLength TermLength { get; set; }
+
+        private readonly IRateService rateService;
+
+        public CDRateTagHelper(IRateService rateService)
+        {
+            this.rateService = rateService;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var rate = rateService.GetCDRateByTerm(TermLength);
+
+            output.Content.SetHtmlContent(
+                $@"<div class=""meter"">
+                    <p> { Title }</p>
+                    <div class=""progress"">
+                        <div class=""progress-bar bg-info"" style=""width: { MeterPercent }%""> { rate }%</div>
+                    </div>
+                </div>");
+        }
+
+    }
+}
