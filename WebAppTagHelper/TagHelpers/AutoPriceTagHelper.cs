520c2a25d60fbafb66bc0e554bf8516c3a2025db
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-29 17:00:55 +0700

    HtmlAttributeNotBound attribute

---
+++ WebAppTagHelper/TagHelpers/AutoPriceTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    public class AutoPriceTagHelper : TagHelper
+    {
+        [HtmlAttributeNotBound]
+        public string Make { get; set; }
+
+        [HtmlAttributeName("model-name")]
+        public string Model { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
