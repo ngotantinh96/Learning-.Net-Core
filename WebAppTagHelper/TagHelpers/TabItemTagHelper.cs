d4c2a61b25e8161bad641f223b0c402cd382d164
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-30 10:05:19 +0700

    pass data between tag helper

---
+++ WebAppTagHelper/TagHelpers/TabItemTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    [RestrictChildren("tab-item")]
+    public class TabItemTagHelper : TagHelper
+    {
+
+        public string Title { get; set; }
+
+        public override void Process(
+            TagHelperContext context, TagHelperOutput output)
+        {
+            string activePage = context.Items["ActivePage"] as string;
+            string activeLink = string.Empty;
+            if (activePage == Title)
+                activeLink = "active";
+
+            output.TagName = "li";
+            var str = string.Format("<a class= 'nav-link {0}' href = '#' >{1}</a>", activeLink, Title);
+            output.Content.SetHtmlContent(str);
+            output.Attributes.Add("class", "nav-item");
+        }
+    }
+}
+
---
+++ WebAppTagHelper/TagHelpers/TabTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebAppTagHelper.TagHelpers
+{
+    [RestrictChildren("tab-item")]
+    public class TabTagHelper : TagHelper
+    {
+        [HtmlAttributeName("active-page")]
+        public string ActivePage { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            context.Items["ActivePage"] = ActivePage;
+
+            output.TagName = "div";
+            output.PreContent
+                .SetHtmlContent("<h3>Silicon Valley Code Camp</h3> <ul class='nav nav-tabs'>");
+            output.PostContent.SetHtmlContent("</ul>");
+            output.Attributes.Add("class", "container");
+        }
+    }
+}
